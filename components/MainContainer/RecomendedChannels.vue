ba662416df5d09b77ce0d110f534df988fd87532
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-04-02 16:57:51 +0200

    feat(components): Impemented components for main container

---
+++ components/MainContainer/RecomendedChannels.vue
@@ -0,0 +1,134 @@
+<template>
+  <section class="recommended-channels">
+    <h2 class="recommended-channels__title">RECOMMENDED CHANNELS</h2>
+    <ul class="recommended-channels__list">
+      <li class="channel-item">
+        <!--   <img
+            src="~/assets/avatar1.jpg"
+            alt="Channel Avatar"
+            class="channel-item__avatar"
+          /> -->
+        <div class="channel-item__info">
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+          <span class="channel-item__game">Just Chatting</span>
+          <span class="channel-item__name">midudev</span>
+        </div>
+        <div class="channel-item__viewers">
+          <span class="channel-item__dot channel-item__dot--online"></span>
+          <span class="channel-item__count">7.2k</span>
+        </div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.recommended-channels {
+  background-color: #18181b;
+  width: 15rem;
+  height: calc(100vh - 3.6em);
+  &__title {
+    color: #efeff1;
+    font-size: 14px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+
+  &__list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+}
+
+.channel-item {
+  display: flex;
+  align-items: center;
+  padding: 6px 0;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+
+  &__avatar {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  &__info {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__name {
+    color: #efeff1;
+    font-size: 14px;
+    font-weight: 500;
+  }
+
+  &__game {
+    color: #adadb8;
+    font-size: 13px;
+  }
+
+  &__viewers {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+
+  &__dot {
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+
+    &--online {
+      background-color: #eb0400;
+    }
+
+    &--offline {
+      background-color: #adadb8;
+    }
+  }
+
+  &__count {
+    color: #adadb8;
+    font-size: 13px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,12 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-
-<script></script>
+<script setup lang="ts">
+import MainContainer from '~/components/MainContainer/MainContainer.vue'
+import RecomendedChannels from '~/components/MainContainer/RecomendedChannels.vue'
+</script>
 
 <template>
-  <header class="header">
-    <nav class="header__nav" aria-label="Main Navigation">
-      <ul class="header__list">
-        <li class="header__item header__left">
-          <img src="~/assets/miduTech.svg" alt="Logo" class="header__logo" />MIDUTCH
-          <img src="~/assets/verticalElipsis.svg" alt="Elipsis" class="header__elipsis" />
-        </li>
-
-        <li class="header__item search">
-          <input type="text" class="search__input" placeholder="Search" />
-          <button class="search__button">
-            <img src="~/assets/search.svg" alt="Search" class="header__search--logo" />
-          </button>
-        </li>
-
-        <li class="header__item header__right">
-          <img src="~/assets/crown.svg" alt="Corona" class="header__icon" />
-          <button class="header__button">
-            <a href="#" class="header__link">Login</a>
-          </button>
-          <button class="header__button header__button--signup">
-            <a href="#" class="header__link">Sign up</a>
-          </button>
-          <img src="~/assets/profile.svg" alt="Profile" class="header__icon" />
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <MainContainer>
+    <RecomendedChannels></RecomendedChannels>
+  </MainContainer>
 </template>
+
+<style lang="scss"></style>
