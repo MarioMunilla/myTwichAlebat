b7a532979b679cbb22295593cb344112f2546345
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-04-08 09:58:30 +0200

    fix(infrastructure): remove lockfile and node_modules to resolve rollup install issue on Vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  ssr: false,
+
+  app: {
+    baseURL: '/myTwichAlebat/',
+  },
+
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/scripts'],
 
   css: ['~/assets/styles/reset.scss'],
@@ -14,6 +20,7 @@ export default defineNuxtConfig({
       },
     },
   },
+
   runtimeConfig: {
     twitchClientSecret: process.env.TWITCH_CLIENT_SECRET,
 
